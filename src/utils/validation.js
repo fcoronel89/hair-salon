99de82d1438d497c47f1a0fbd9ffd3db7804079c
Federico Coronel <fedec34@gmail.com>
2023-10-16 12:25:09 -0300

    add image format and size validation in form

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,5 +1,8 @@
 import * as Yup from "yup";
 
+const supportedImageFormats = ["image/jpeg", "image/png", "image/gif"]; // Supported image formats
+const maxImageSizeInBytes = 20 * 1024 * 1024; // Maximum image size (20 MB)
+
 export const isRequired = (message) => Yup.string().required(message);
 export const isNumber = (message) =>
   Yup.number()
@@ -12,7 +15,19 @@ export const hasAtLeastOneChecked = (message) =>
   Yup.object().test("atLeastOneChecked", message, (values) =>
     Object.values(values).some(Boolean)
   );
-export const isImage = (message) => Yup.mixed().required(message);
+export const isImage = (message) =>
+  Yup.mixed()
+    .test("fileFormat", "Formato de imagen incorrecto", (value) => {
+      if (!value) return true; // Allow empty values (no file selected)
+
+      return supportedImageFormats.includes(value.type);
+    })
+    .test("fileSize", "Imagen muy grande", (value) => {
+      if (!value) return true; // Allow empty values (no file selected)
+
+      return value.size <= maxImageSizeInBytes;
+    })
+    .required(message);
 export const isDNI = (message) =>
   Yup.string().max(8, "DNI invalido").min(7, "DNI invalido").required(message);
 export const isEmail = (message) =>
