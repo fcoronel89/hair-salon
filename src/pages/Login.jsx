228d0a8436789a14e5eb73ba4c4650a502ddb817
Federico Coronel <fedec34@gmail.com>
2023-10-19 12:53:35 -0300

    add validation if user is inactive in login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,25 +8,39 @@ const LoginPage = () => {
 
 export default LoginPage;
 
-export const action = async ({ request }) => {
+const extractFormData = async (request) => {
   const formData = await request.formData();
-  const userName = formData.get("userName");
-  const password = formData.get("password");
+  return {
+    userName: formData.get("userName"),
+    password: formData.get("password"),
+  };
+};
 
+const setLocalStorageTokens = (user) => {
+  localStorage.setItem("token", user.userName);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 5); // 5 hours of session
+  localStorage.setItem("tokenExpiration", expiration);
+
+  if (user.userType === "admin") {
+    localStorage.setItem("admin", true);
+  } else {
+    localStorage.removeItem("admin");
+  }
+};
+
+export const action = async ({ request }) => {
   try {
+    const { userName, password } = await extractFormData(request);
+
     const user = await login({ userName, password });
 
     if (user) {
-      localStorage.setItem("token", user.userName);
-      const expiration = new Date();
-      expiration.setHours(expiration.getHours() + 5); // 5 hours of session
-      localStorage.setItem("tokenExpiration", expiration);
-
-      if (user.userType === "admin") {
-        localStorage.setItem("admin", true);
-      } else {
-        localStorage.removeItem("admin");
+      if (!user.active) {
+        return { message: "Usuario inactivo, contacte al admin" };
       }
+
+      setLocalStorageTokens(user);
 
       return redirect("/agenda");
     } else {
