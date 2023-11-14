e2dc5a68da90cc5c84983d586cb648e8b4c04681
fcoronel89 <fedec34@gmail.com>
2023-11-14 18:25:57 -0300

    refactor http file

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,393 +1,113 @@
 import moment from "moment";
 import { apiUrl } from "./helpers";
 
-/*Export functions*/
+const apiRequest = async (url, method, data) => {
+  try {
+    const response = await fetch(url, {
+      method,
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data ? JSON.stringify(data) : undefined,
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      throw new Error("Request failed");
+    }
+  } catch (error) {
+    console.log("Error:", error);
+    throw error;
+  }
+};
 
 /***Professional***/
 
 export const createProfessional = async (professionalData) => {
-  try {
-    const response = await fetch(`${apiUrl}/professional`, {
-      method: "POST", // or 'PATCH' depending on your API
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(professionalData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw new Error(error);
-  }
+  return await apiRequest(`${apiUrl}/professional`, "POST", professionalData);
 };
 
 export const updateProfessional = async (professionalData, professionalId) => {
-  try {
-    const response = await fetch(`${apiUrl}/professional/${professionalId}`, {
-      method: "PUT", // or 'PATCH' depending on your API
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(professionalData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw new Error(error);
-  }
+  return await apiRequest(
+    `${apiUrl}/professional/${professionalId}`,
+    "PUT",
+    professionalData
+  );
 };
 
 export const getProfessionalById = async (professionalId) => {
-  try {
-    const response = await fetch(`${apiUrl}/professional/${professionalId}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      const birthDate = data.birthDate && new Date(data.birthDate);
-      const professional = {
-        ...data,
-        birthDate: birthDate && moment(birthDate).format("YYYY-MM-DD"),
-      };
-      return professional;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  const data = await apiRequest(
+    `${apiUrl}/professional/${professionalId}`,
+    "GET"
+  );
+  const birthDate = data.birthDate && new Date(data.birthDate);
+  const professional = {
+    ...data,
+    birthDate: birthDate && moment(birthDate).format("YYYY-MM-DD"),
+  };
+  return professional;
 };
 
 export const getProfessionals = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/professionals`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Professionals");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/professionals`, "GET");
 };
 
 /***Client***/
 
 export const getClientbyPhone = async (phone) => {
-  try {
-    const response = await fetch(`${apiUrl}/clients?phone=${phone}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Clients");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/clients?phone=${phone}`, "GET");
 };
 
 export const getClientbyId = async (clientId) => {
-  try {
-    const response = await fetch(`${apiUrl}/client/${clientId}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load client");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/client/${clientId}`, "GET");
 };
 
 export const createClient = async (clientData) => {
-  try {
-    const response = await fetch(`${apiUrl}/client`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(clientData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    return error;
-  }
+  return await apiRequest(`${apiUrl}/client`, "POST", clientData);
 };
 
 /***User***/
 
 export const getUsers = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/users`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Users");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/users`, "GET");
 };
 
 /***Services***/
 
 export const getServices = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/services`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load services");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.log("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/services`, "GET");
 };
 
 /***Shift***/
 
 export const createShift = async (shiftData) => {
-  try {
-    const response = await fetch(`${apiUrl}/shift`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(shiftData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    return error;
-  }
+  return await apiRequest(`${apiUrl}/shift`, "POST", shiftData);
 };
 
 export const updateShift = async (shiftData, shiftId) => {
-  try {
-    const response = await fetch(`${apiUrl}/shift/${shiftId}`, {
-      method: "PUT",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(shiftData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    return error;
-  }
+  return await apiRequest(`${apiUrl}/shift/${shiftId}`, "PUT", shiftData);
 };
 
 export const updateShiftConfirmation = async (shiftData, shiftId) => {
-  try {
-    const response = await fetch(`${apiUrl}/shift/confirm-shift/${shiftId}`, {
-      method: "PUT",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(shiftData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    return error;
-  }
+  return await apiRequest(
+    `${apiUrl}/shift/confirm-shift/${shiftId}`,
+    "PUT",
+    shiftData
+  );
 };
 
 export const getShifts = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/shifts`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Users");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.log("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/shifts`, "GET");
 };
 
 export const getShiftbyId = async (shiftId) => {
-  try {
-    const response = await fetch(`${apiUrl}/shift/${shiftId}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Users");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/shift/${shiftId}`, "GET");
 };
 
 export const deleteShift = async (shiftId) => {
-  try {
-    const response = await fetch(`${apiUrl}/shift/${shiftId}`, {
-      method: "DELETE",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("failed load Users");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    throw error;
-  }
+  return await apiRequest(`${apiUrl}/shift/${shiftId}`, "DELETE");
 };
 
 export const sendMessageToConfirmShift = async (shift, confirmationType) => {
@@ -399,27 +119,8 @@ export const sendMessageToConfirmShift = async (shift, confirmationType) => {
     professional,
   };
   console.log(data, "data");
-  //Call Backend
-  const response = await fetch(
-    `${apiUrl}/send-whatsapp-message`,
-    {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
 
-  if (!response.ok) {
-    const error = new Error("Error en la solicitud");
-    error.code = response.status;
-    error.info = await response.json();
-    throw error;
-  }
-
-  return response.json();
+  return await apiRequest(`${apiUrl}/send-whatsapp-message`, "POST", data);
 };
 
 export const confirmShift = async (shiftId, confirmationType) => {
@@ -429,109 +130,33 @@ export const confirmShift = async (shiftId, confirmationType) => {
   } else {
     updatedField = { clientConfirmed: true };
   }
-  
+
   await updateShiftConfirmation(updatedField, shiftId);
-  /*if (confirmationType === "professional") {
-    await sendMessageToConfirmShift(shift, "client");
-  }*/
   return true;
 };
 
-//Api methods
+// Api methods
 
 export const getUserById = async (userId) => {
-  try {
-    const response = await fetch(`${apiUrl}/user/${userId}`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!response.ok) {
-      // Handle errors, such as when the user is not found
-      throw new Error("User not found");
-    }
+  const data = await apiRequest(`${apiUrl}/user/${userId}`, "GET");
 
-    const data = await response.json();
-    const birthDate = data.user.birthDate && new Date(data.user.birthDate);
-    const user = {
-      ...data.user,
-      birthDate: birthDate && moment(birthDate).format("YYYY-MM-DD"),
-    };
+  const birthDate = data.user.birthDate && new Date(data.user.birthDate);
+  const user = {
+    ...data.user,
+    birthDate: birthDate && moment(birthDate).format("YYYY-MM-DD"),
+  };
 
-    // Handle the user data retrieved from the backend
-    return user;
-  } catch (error) {
-    // Handle any other errors that may occur during the request
-    console.log("Error:", error);
-    throw new Error(error); // Or return an error object
-  }
+  return user;
 };
 
 export const updateUser = async (userId, userData) => {
-  try {
-    const response = await fetch(`${apiUrl}/user/${userId}`, {
-      method: "PUT", // or 'PATCH' depending on your API
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(userData),
-    });
-
-    if (response.ok) {
-      // User updated successfully
-      const data = await response.json();
-      return data;
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    // Handle any network or other errors
-    console.error("Error:", error);
-    return error;
-  }
+  return await apiRequest(`${apiUrl}/user/${userId}`, "PUT", userData);
 };
 
 export const logout = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/auth/logout`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      // User updated successfully
-      return await response.json();
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    throw new Error("Logout failed");
-  }
+  return await apiRequest(`${apiUrl}/auth/logout`, "GET");
 };
 
 export const isLoggedIn = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/auth/isLoggedIn`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.ok) {
-      return await response.json();
-    } else {
-      // Handle errors
-      throw new Error("User update failed");
-    }
-  } catch (error) {
-    throw new Error("Logout failed");
-  }
+  return await apiRequest(`${apiUrl}/auth/isLoggedIn`, "GET");
 };
