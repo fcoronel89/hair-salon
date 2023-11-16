9f93a1ebe1664571f31781603ee7001ad3103637
fcoronel89 <fedec34@gmail.com>
2023-11-16 14:11:01 -0300

    refactor helpers file

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,55 +1,72 @@
 export const apiUrl = import.meta.env.VITE_BACKEND_URL;
 
-// Function to remove query parameters
+// Remove query parameters from a URL
 export function removeQueryParameters(url) {
+  // Find the index of the question mark in the URL
   const indexOfQuestionMark = url.indexOf("?");
+
+  // If the question mark exists in the URL
   if (indexOfQuestionMark !== -1) {
+    // Return the substring of the URL before the question mark
     return url.substring(0, indexOfQuestionMark);
   }
+
+  // If the question mark does not exist in the URL, return the original URL
   return url;
 }
 
+// Get the date object for yesterday
 export const getYesterdayDate = () => {
+  // Get the current date object
   const currentDate = new Date();
+
+  // Calculate the number of milliseconds in one day
   const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+
+  // Subtract one day from the current date to get the previous date
   const previousDate = new Date(currentDate.getTime() - oneDayInMilliseconds);
+
+  // Return the previous date
   return previousDate;
 };
 
+// Get the date object in the local timezone
 export const getDateInLocalTimezone = (date) => {
-  let originalDate = new Date(date);
+  // Create a new date object from the input date
+  const originalDate = new Date(date);
 
-  // Add 3 hours to the original date
-  let dateObject = new Date(originalDate);
+  // Create a new date object with the same time as the original date
+  const dateObject = new Date(originalDate);
+
+  // Add 3 hours to the date object to convert it to the local timezone
   dateObject.setHours(originalDate.getHours() + 3);
+
+  // Return the date object in the local timezone
   return dateObject;
-}
+};
 
+// Combine a date object and a time string into a single date object
 export const getCombinedDateTime = (date, timeString) => {
-
-  // Add 3 hours to the original date
+  // Get the date object in the local timezone
   const dateObject = getDateInLocalTimezone(date);
-  // Extract date components
+
+  // Get the year, month, and day from the date object
   const year = dateObject.getFullYear();
   const month = dateObject.getMonth();
   const day = dateObject.getDate();
 
-  // Extract time components
+  // Split the time string into hours and minutes
   const [hours, minutes] = timeString.split(":").map(Number);
 
-  // Combine date and time into a single Date object
+  // Create a new date object with the combined date and time
   return new Date(year, month, day, hours, minutes);
 };
 
+// Add minutes to a date object
 export const addMinutesToDate = (date, minutesToAdd) => {
-  return new Date(date.getTime() + minutesToAdd * 60000);
-};
+  // Calculate the number of milliseconds to add based on the number of minutes
+  const millisecondsToAdd = minutesToAdd * 60000;
 
-export const formatServices = (services) => {
-  return services
-    ? Object.entries(services).map(([id, service]) => ({
-        id,
-        services: service,
-      }))[1].services
-    : null;
+  // Add the milliseconds to the date object to get the new date
+  return new Date(date.getTime() + millisecondsToAdd);
 };
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,15 +1,23 @@
+// Import dependencies
 import { string, number, date, mixed } from "yup";
 
+// Define supported image formats and max image size
 const supportedImageFormats = new Set(["image/jpeg", "image/png", "image/gif"]);
 const maxImageSizeInBytes = 20 * 1024 * 1024;
 
+// Validation functions
 export const isRequired = (message) => string().required(message);
 
 export const isNumber = (message) =>
-  number().integer("Ingresar solo numeros").moreThan(99999999, "Ingresar numero valido").required(message);
+  number()
+    .integer("Ingresar solo numeros")
+    .moreThan(99999999, "Ingresar numero valido")
+    .required(message);
 
 export const isDate = (message) =>
-  date().nullable().max(new Date(), message);
+  date()
+    .nullable()
+    .max(new Date(), message);
 
 export const hasAtLeastOneChecked = (message) =>
   mixed().test("atLeastOneChecked", message, (values) => {
@@ -34,10 +42,3 @@ export const isDNI = (message) =>
 
 export const isEmail = (message) =>
   string().email("Ingresa un Email valido").required(message);
-
-export const isPassword = (message) =>
-  string()
-    .required(message)
-    .min(6, "La contraseña debe tener al menos 6 caracteres")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/, "La contraseña debe tener al menos una miniscula, una mayuscula, un numero y un caracter especial no comillas");
-
