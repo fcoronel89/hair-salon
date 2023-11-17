62e2da8d4d0f4b8ec3d90b11569e2f758524e49b
fcoronel89 <fedec34@gmail.com>
2023-11-17 12:40:22 -0300

    migrate users component to typescript

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,8 +1,10 @@
 import { Link, useLoaderData } from "react-router-dom";
 import classes from "./Users.module.css";
+import User from "../models/user";
+import React from "react";
 
-const getUserTypeText = (userType) => {
-  const userTypeMap = {
+const getUserTypeText = (userType: string): string => {
+  const userTypeMap: Record<string, string> = {
     seller: "Vendedor",
     hairsalon: "Peluqueria",
     admin: "Administrador",
@@ -11,8 +13,23 @@ const getUserTypeText = (userType) => {
   return userTypeMap[userType] || "Unknown"; // Default to "Unknown" for unrecognized types
 };
 
-const Users = () => {
-  const users = useLoaderData();
+interface UserRowProps {
+  user: User;
+}
+
+const UserRow: React.FC<UserRowProps> = ({ user }) => (
+  <tr key={user._id}>
+    <td>{user.firstName}</td>
+    <td>{user.lastName}</td>
+    <td>{getUserTypeText(user.userType)}</td>
+    <td>
+      <Link to={`/usuarios/editar/${user._id}`}>Editar</Link>
+    </td>
+  </tr>
+);
+
+const Users: React.FC  = () => {
+  const users: User[] | unknown = useLoaderData();
 
   return (
     <div className={classes.container}>
@@ -26,17 +43,7 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-          {users &&
-            users.map((user) => (
-              <tr key={user._id}>
-                <td>{user.firstName}</td>
-                <td>{user.lastName}</td>
-                <td>{getUserTypeText(user.userType)}</td>
-                <td>
-                  <Link to={`/usuarios/editar/${user._id}`}>Editar</Link>
-                </td>
-              </tr>
-            ))}
+          {users && users.map((user:User) => <UserRow key={user._id} user={user} />)}
         </tbody>
       </table>
       <ul></ul>
