64f8e1180f2e54d23c1a5d7b0e19a1ea1131a714
fcoronel89 <fedec34@gmail.com>
2023-11-22 18:32:52 -0300

    Add search users in users grid page

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,7 +1,7 @@
 import { Link, useLoaderData } from "react-router-dom";
 import classes from "./Users.module.css";
 import User from "../models/user";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const getUserTypeText = (userType: string): string => {
   const userTypeMap: Record<string, string> = {
@@ -28,24 +28,58 @@ const UserRow: React.FC<UserRowProps> = ({ user }) => (
   </tr>
 );
 
-const Users: React.FC  = () => {
-  const users: User[] | unknown = useLoaderData();
+const Users: React.FC = () => {
+  const usersData: User[] | unknown = useLoaderData();
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const [users, setUsers] = useState<User[]>(usersData);
+
+  const handleSearch = () => {
+    const searchText = searchInputRef.current?.value.trim().toLowerCase();
+    if (searchText) {
+      const filteredUsers = (users as User[]).filter((user) => {
+        const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+        return fullName.startsWith(searchText);
+      });
+      setUsers(filteredUsers);
+    }
+  };
+
+  const handleClearSearch = () => {
+    setUsers(usersData);
+    if (searchInputRef.current) {
+      searchInputRef.current.value = "";
+    }
+  };
 
   return (
     <div className={classes.container}>
-      <table className={classes["users-list"]}>
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Tipo de Usuario</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users && users.map((user:User) => <UserRow key={user._id} user={user} />)}
-        </tbody>
-      </table>
+      <div className={classes["search-container"]}>
+        <input
+          ref={searchInputRef}
+          type="text"
+          placeholder="Por nombre y apellido"
+        />
+        <button onClick={handleSearch}>Buscar usuarios</button>
+        <button onClick={handleClearSearch}>Limpiar busqueda</button>
+      </div>
+      {users.length ? (
+        <table className={classes["users-list"]}>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Tipo de Usuario</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users &&
+              users.map((user: User) => <UserRow key={user._id} user={user} />)}
+          </tbody>
+        </table>
+      ) : (
+        <p className={classes.empty}>No hay resultados</p>
+      )}
       <ul></ul>
     </div>
   );
